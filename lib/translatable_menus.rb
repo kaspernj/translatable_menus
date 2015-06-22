c68f880fa7ca355d7a14ab17cc7bcd4a44ccb302
kaspernj <k@spernj.org>
2015-06-22 14:54:54 +0200

    Removed unnecessary require

--- lib/translatable_menus.rb
+++ lib/translatable_menus.rb
@@ -2,7 +2,6 @@ require_relative "translatable_menus/engine"
 require "string-cases"
 require_relative "translatable_menus/active_admin_integrator"
 require_relative "translatable_menus/connections"
-require_relative "../app/models/translatable_menus/translatable_menu.rb"
 
 # Add the translation files to the I18n load path.
 locales_path = Dir["#{File.dirname(__FILE__)}/../config/locales/**/*.yml"]
